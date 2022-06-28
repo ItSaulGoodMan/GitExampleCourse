c0dbb662ce4133e8c471ae00c651d9cf7c9026c5
Nemanja Pejic <nemanjap@domain.local>
2022-06-28 14:45:00 +0200

    new hello world

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 print("===== Welcome to Calculator App =====\n")
 while 1:
-    print("What would you like to do:-")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3  Multiplication")
-    print("4. Division")
-    print("5. Exit")
+    #print("What would you like to do:-")
+    #print("1. Addition")
+    #print("2. Subtraction")
+    #print("3  Multiplication")
+    #print("4. Division")
+    #print("5. Exit")
     choice = int(input("Enter your choice: "))
     if choice == 1:
         #num1 = int(input("Enter first number: "))
